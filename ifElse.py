cd8d56bf03966e8e5062ff8b2fee2c2b944a3c85
Numir <numirselman@gmail.com>
2020-06-08 21:47:08 +0200

    Update ifElse.py

--- ifElse.py
+++ ifElse.py
@@ -1,10 +1,34 @@
-number = 3.4
 
-if number > 0:
+"PrOgRaMiZ".upper()
+"PrOgRaMiZ".lower()
+'Happy New Year'.replace('Happy', 'Brilliant')
+
+List = []
+print("Blank List: ")
+print(List)
+
+# Creating a List of numbers
+List = [10, 20, 14]
+print("\nList of numbers: ")
+print(List)
+
+# Creating a List of strings and accessing
+# using index
+List = ["Geeks", "For", "Geeks"]
+print("\nList Items: ")
+print(List[0])
+print(List[2])
+
+# Creating a Multi-Dimensional List
+# (By Nesting a list inside a List)
+List = [['Geeks', 'For'], ['Geeks']]
+print("\nMulti-Dimensional List: ")
+print(List)
+
+say = 3.4
+if say > 0:
     print("Pozitif")
-elif number == 0:
+elif say == 0:
     print("Sıfır")
 else:
     print("Negativ")
-"PrOgRaMiZ".upper()
-"PrOgRaMiZ".lower()
